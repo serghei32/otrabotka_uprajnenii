e238407f0d9f69b2c8e3d2342065b2508a103a93
serghei32 <115115828+serghei32@users.noreply.github.com>
2025-02-11 13:15:30 +0100

    Split()

---
+++ javaScript/script2.js
@@ -0,0 +1,10 @@
+const planet = document.getElementById('green')
+console.log(planet);
+planet.innerHTML='ale'
+// planet.textContent='SSsale'
+
+const newElem1=document.createElement('p')
+newElem1.textContent='respons'
+document.body.appendChild(newElem1)
+
+console.dir(newElem1)
